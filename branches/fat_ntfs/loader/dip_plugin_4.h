c08476196fb28c2641832a9d83f674c4e1629e7e
e.bovendeur@gmail.com <e.bovendeur@gmail.com@926e4b9c-16c7-1153-8a79-ddbb8f96e676>
2010-03-11 16:31:47 +0000

    Removed fat_ntfs branch, due to incomplete commit.

--- branches/fat_ntfs/loader/dip_plugin_4.h
+++
@@ -1,3 +0,0 @@
-#define size_dip_plugin_4 3080
-
-extern unsigned char dip_plugin_4[3080];
--- branches/fat_ntfs/loader/load_dol.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef _LOAD_DOL_H_
-#define _LOAD_DOL_H_
-
-u32 load_dol(const void *dol_data, int dol_len, void (*f)(void *, int, void *), void *userData);
-
-#endif
